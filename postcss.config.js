cc6123b27f4f48e3aed7c653c12f30bc11144d7d
frank.gairal <frank@autofi.io>
2019-03-23 11:28:50 -0700

    fix(css): replace styled component by tailwind

---
+++ postcss.config.js
@@ -0,0 +1,13 @@
+const tailwindcss = require('tailwindcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const reporter = require('postcss-reporter');
+
+module.exports = {
+  plugins: [
+    tailwindcss('./tailwind.js'),
+    autoprefixer,
+    cssnano,
+    reporter({ clearReportedMessages: true }),
+  ],
+};
